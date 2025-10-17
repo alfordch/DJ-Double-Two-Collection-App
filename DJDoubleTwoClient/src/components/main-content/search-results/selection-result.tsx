2ce79c444aa9343cf642affb41135471baa18825
djdoubletwo <alfordch@umich.edu>
2025-10-17 18:39:06 -0500

    Added dropdown with actions for selection-result

--- DJDoubleTwoClient/src/components/main-content/search-results/selection-result.tsx
+++ DJDoubleTwoClient/src/components/main-content/search-results/selection-result.tsx
@@ -1,19 +1,44 @@
-import { ListMusic } from "lucide-react"
+import { ListMusic, Ellipsis, Plus, Share, Trash2, PencilLine } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu"
 
 export default function SelectionResult({ selection }: { selection: any}) {
-    return (
-        <div className="flex items-center justify-between p-3 rounded-md border shadow-sm hover:bg-muted transition">
+   return (
+      <div className="flex items-center justify-between p-3 rounded-md border shadow-sm hover:bg-muted transition">
          <div className="flex items-center gap-3">
             <div className="relative flex-shrink-0 bg-cover bg-no-repeat">
-                    <ListMusic className="w-25 h-25 bg-accent rounded-lg p-1 text-muted-foreground drop-shadow-lg" />
-                </div>
-                <div className="flex flex-col">
-                    {selection.selectionName}
-                    {selection.added && (
-                        <p className="text-sm text-muted-foreground leading-tight">Already added to this selection</p>
-                    )}
-                </div>
+                     <ListMusic className="w-25 h-25 bg-accent rounded-lg p-1 text-muted-foreground drop-shadow-lg" />
             </div>
-        </div>
-    )
+            <div className="flex flex-col flex-1 min-w-0">
+                  <p className="font-bold leading-tight">{selection.selectionName}</p>
+            </div>
+         </div>
+         <div className="text-lg font-bold drop-shadow-lg">
+            <DropdownMenu modal={false}>
+               <DropdownMenuTrigger asChild>
+                  <Button type="button" variant="outline" className="cursor-pointer">
+                     <Ellipsis />
+                  </Button>
+               </DropdownMenuTrigger>
+               <DropdownMenuContent className="w-40" align="end">
+                  <DropdownMenuGroup>
+                     <DropdownMenuItem className="cursor-pointer"><Plus />Add New Tracks</DropdownMenuItem>
+                     <DropdownMenuItem className="cursor-pointer"><PencilLine />Rename</DropdownMenuItem>
+                     <DropdownMenuItem className="cursor-pointer"><Share />Share</DropdownMenuItem>
+                     <DropdownMenuSeparator />
+                     <DropdownMenuItem className="text-red-500 font-bold cursor-pointer"><Trash2 className="text-red-500" />Delete</DropdownMenuItem>
+                  </DropdownMenuGroup>
+               </DropdownMenuContent>
+            </DropdownMenu>
+         </div>
+      </div>
+   )
 }
