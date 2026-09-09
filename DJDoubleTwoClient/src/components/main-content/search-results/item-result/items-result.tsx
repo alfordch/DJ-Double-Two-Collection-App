57a2c1756e08d10bfc473174bf256d2beb3bd04c
djdoubletwo <alfordch@umich.edu>
2026-09-08 20:00:34 -0500

    Update item-result with new webGraphics directory

--- DJDoubleTwoClient/src/components/main-content/search-results/item-result/items-result.tsx
+++ DJDoubleTwoClient/src/components/main-content/search-results/item-result/items-result.tsx
@@ -10,39 +10,36 @@ import ViewTracks from "@/components/main-content/search-results/item-result/vie
 export default function ItemResult({ item }: { item: any }) {
    const [results, setResults] = useState<any[]>([])
    const [error, setError] = useState("")
-
+   const [imgError, setImgError] = useState(false)
+   
    const handleViewTracks = async() => {
       let res
       setError("")
-      
       try {
          res = await fetch(`/tracks/searchTracksByItem?q=${encodeURIComponent(item.ItemID)}`)
-         
          if (!res.ok) {
             throw new Error("Fetch Failed")
          }
-
          const data = await res.json()
          setResults(data)
       }
-
       catch (err) {
          console.error(err)
          setError("Error Encountered In Call To thedungeon0000 API, Please Try Again Later")
       }
    }
-
+   
    return (
       <Dialog>
          <DialogTrigger asChild>
             <div className="flex items-center justify-between p-3 rounded-md border shadow-sm hover:bg-muted transition cursor-pointer" onClick={handleViewTracks}>
                <div className="flex items-center gap-3">
                   <div className="relative flex-shrink-0 bg-cover bg-no-repeat">
-                     {item.ItemCoverImage !== null ?
-                        <img src={`/covers/${item.ItemCoverImage}`} alt={`${item.ItemName} cover`} className="w-25 h-25 rounded-lg drop-shadow-lg"/>
-                     :
-                        <Disc3 className="w-10 h-10 bg-accent rounded-lg p-1 text-muted-foreground" />
-                     }
+                     {!imgError ? (
+                        <img src={`/webGraphics/${item.ItemID}/${item.ItemID}.webp`} alt={`${item.ItemName} cover`} onError={() => setImgError(true)} className="w-25 h-25 rounded-lg drop-shadow-lg border-3 border-black"/>
+                     ) : (
+                        <img src={`/fallbackGraphics/${item.ItemCoverImage}`} alt={`${item.ItemName} cover`} onError={() => setImgError(true)} className="w-25 h-25 rounded-lg drop-shadow-lg border-3 border-black"/>
+                     )}
                   </div>
                   <div className="flex flex-col flex-1 min-w-0">
                      <p className="font-bold leading-tight">{item.ItemName}</p>
